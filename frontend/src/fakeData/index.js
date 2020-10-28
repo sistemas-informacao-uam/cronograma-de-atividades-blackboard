22e47c6f4413395589cc8a9e2998195328f7a590
paulohdelia <paulohdelia@gmail.com>
2020-10-28 01:54:59 -0300

    add some fake data to use in the presentation

---
+++ frontend/src/fakeData/index.js
@@ -0,0 +1,61 @@
+export const subjectsListFakeData = [
+  'Desenvolvimento Web',
+  'Engenharia de Software',
+  'Prática de Banco de Dados',
+  'Pesquisa, Ordenação e Técnicas de Armazenamento',
+]
+
+export const activitiesListFakeData = [
+  {
+    date: '28 de Outubro',
+    activities: [
+      {
+        id: 1,
+        type: 'apresentacao',
+        subject: 'Desenvolvimento Web',
+        title: 'Apresentação deste trabalho que você está vendo agora',
+      }
+    ],
+  },
+  {
+    date: '30 de Outubro',
+    activities: [
+      {
+        id: 1,
+        type: 'trabalho',
+        subject: 'Pesquisa, Ordenação e Técnicas de Armazenamento',
+        title: 'Quick sort, Merge Sort, Whatever Sort',
+      },
+      {
+        id: 2,
+        type: 'prova',
+        subject: 'Engenharia de Software',
+        title: 'Diagramas',
+      },
+    ],
+  },
+  {
+    date: '17 de Novembro',
+    activities: [
+      {
+        id: 1,
+        type: 'apresentacao',
+        subject: 'Pesquisa, Ordenação e Técnicas de Armazenamento',
+        title: 'Muitos algoritmos',
+      },
+      {
+        id: 2,
+        type: 'trabalho',
+        subject: 'Desenvolvimento Web',
+        title: 'HTML, CSS e JS',
+      },
+      {
+        id: 3,
+        type: 'prova',
+        subject: 'Prática de Banco de Dados',
+        title: 'UPDATE Alunos SET nota = 10',
+      },
+
+    ],
+  }
+]
